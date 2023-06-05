b174b297969ff34cfccac5c9234337d1acceeb48
Odin H <76893858+odiinh@users.noreply.github.com>
2023-06-05 17:53:53 +0100

    Store Found URLS
    When a working URL is found, it adds it to the extensions local extension for quicker recall and less strain on third party servers.

--- revision-unlocker/foreground.js
+++ revision-unlocker/foreground.js
@@ -1,35 +1,41 @@
 (async () => {
     var url = window.location.href;
     if (String(url).match(/^https:\/\/qualifications\.pearson\.com\/en\/qualifications\/.*Exam-materials$/g) != null) {
+        var fileNames = []
+        var aElements = []
         document.querySelectorAll('.expandFilters')[1].children[0].click();
         document.querySelectorAll('.docmargin').forEach(async function (el) {
             el.querySelectorAll('a').forEach(async function (el2) {
                 if (el2.id.match(/secure/gm) != null && el2.href.match(/pdf/gm) != null && el2.href.match(/pef/gm) == null) {
-                    var baseURL = 'https://dynamicpapers.com/wp-content/uploads/';
-                    var filename = el2.href.match(/(?<=\/exam-materials\/).*?(?=\?)/gm)[0].replace("_@_",".")
-                    var year = filename.match(/\d{4}(?=\d{2}\d{2}\.pdf)/gm)[0];
-                    var month = filename.match(/(?<=\d{4})\d{2}(?=\d{2}\.pdf)/gm)[0];
-                    if (month != "01") {
-                        month = String(parseInt(month) - 1).padStart(2, '0')
-                    }
-                    var newURL = baseURL + year + '/' + month + '/' + filename;
-                    var port = chrome.runtime.connect({ name: "urlTransfer" });
-                    await port.postMessage({ test: false, url: newURL, month: month, year: year})
-                    var listener = port.onMessage.addListener(async function (msg) {
-                        if (msg.confirmedURL != "") {
-                            console.log(msg)
-                            el2.href = msg.confirmedURL
-                            var titleEl = el2.querySelectorAll(".textContent")[0]
-                            var padlockEl = el2.querySelectorAll("img.padlock")[0]
-                            titleEl.textContent = titleEl.textContent + " (Unlocked)"
-                            padlockEl.src = chrome.runtime.getURL("images/icon_padlock_Blue.png")
-                            await port.disconnect()
-                        }
-                    })
-                    
+                    aElements.push(el2)
+                    var fileName = el2.href.match(/(?<=\/exam-materials\/).*?(?=\?)/gm)[0].replace("_@_",".")
+                    var year = fileName.match(/\d{4}(?=\d{2}\d{2}\.pdf)/gm)[0];
+                    var month = fileName.match(/(?<=\d{4})\d{2}(?=\d{2}\.pdf)/gm)[0];
+                    fileNames.push({fileName:fileName, month: month, year: year})                    
                 }
             });
         });
-
+        
+        var port = chrome.runtime.connect({ name: "urlTransfer" });
+        console.log(fileNames)
+        await port.postMessage({test:false,fileNames:fileNames})
+        var listener = port.onMessage.addListener(async function (msg) {
+            if (msg.returnedURLS) {
+                console.log(msg)
+                for (let index = 0; index < Object.keys(msg.returnedURLS).length; index++) {
+                    if (msg.returnedURLS[index] != "") {
+                        aElements[index].href = msg.returnedURLS[index]
+                        var titleEl = aElements[index].querySelectorAll(".textContent")[0]
+                        var padlockEl = aElements[index].querySelectorAll("img.padlock")[0]
+                        titleEl.textContent = titleEl.textContent + " (Unlocked)"
+                        padlockEl.src = chrome.runtime.getURL("images/icon_padlock_Blue.png")
+                    }   
+                    else {
+                        console.log("No URL found for " + fileNames[index].fileNames)
+                    }                    
+                }
+                await port.disconnect()
+            }
+        })
     }
 })();
--- revision-unlocker/service-worker.js
+++ revision-unlocker/service-worker.js
@@ -1,60 +1,98 @@
 (async () => {
-	chrome.runtime.onConnect.addListener(async function(port) {
+	await chrome.runtime.onConnect.addListener(async function(port) {
 		console.assert(port.name == "urlTransfer");
-		port.onMessage.addListener(async function(msg) {
+		await port.onMessage.addListener(async function(msg) {
+			console.log("New Message:" + JSON.stringify(msg))
 			if (msg.test == true) {
-				port.postMessage({ confirmedURL: "pong" });
+				console.log("TEST: Pong!")
 				return true;
 			}
-			else if (msg.url) {
-				var URL = msg.url
-				var year = msg.year
-				var month = msg.month
-				var currentMonth = String(new Date().getMonth() + 1).padStart(2,"0")
-				var currentYear = String(new Date().getFullYear())
-				var found = false
-				var count = 0
-				while (found == false && count < 5) {
-					var request = await fetch(URL, {
-						"headers": {
-							"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-							"accept-language": "en-US,en;q=0.9,en-GB;q=0.8",
-							"sec-ch-ua-mobile": "?0",
-							"sec-fetch-dest": "document",
-							"sec-fetch-mode": "navigate",
-							"sec-fetch-site": "none",
-							"sec-fetch-user": "?1",
-							"upgrade-insecure-requests": "1"
-						},
-						"referrerPolicy": "strict-origin-when-cross-origin",
-						"body": null,
-						"method": "HEAD",
-						"mode": "cors",
-						"credentials": "include"
-					});
-					if (request.status == 200) {
-						port.postMessage({ confirmedURL: URL });
-						found = true
-						return true;
+			else if (msg.fileNames != []) {
+				console.log("i see some filenames")
+				var storedUrls = {}
+				var search = await chrome.storage.local.get("storedUrls")
+				if (search != {}) {
+					storedUrls = search.storedUrls
+				}
+				
+				var storedUrlKeys = Object.keys(storedUrls)
+				var storedUrlValues = Object.values(storedUrls)
+				var returnedURLS = {}
+				for (let index = 0; index < msg.fileNames.length; index++) {
+					let urlInfo = msg.fileNames[index]
+					var fileStorageIndex = storedUrlKeys.indexOf(urlInfo.fileName)
+					if (fileStorageIndex != -1){
+						returnedURLS[index]=storedUrlValues[fileStorageIndex]
 					}
-					else if (month == currentMonth && year == currentYear) {
-						port.postMessage({ confirmedURL: "" });
-						return true;
+					else {
+						var searchedURL = await findURL(urlInfo).then(url => {
+							console.log(url)
+							if (url != "") {
+								storedUrls[urlInfo.fileName] = url
+							}
+							returnedURLS[index]=url
+						})
 					}
-					else if (request.status == 404) {
-						URL = URL.split('/')
-						month = String(parseInt(month) + 1).padStart(2, '0')
-						if (parseInt(month) % 12 == 1) {
-							month = '01'
-							year = String(parseInt(year) + 1)
-							URL[URL.length - 3] = year
-						}
-						URL[URL.length - 2] = month
-						URL = URL.join("/")
-						count++
+					if (index == msg.fileNames.length-1) {
+						await port.postMessage({returnedURLS:returnedURLS})
+						await chrome.storage.local.set({storedUrls})
 					}
+
 				}
 			}
 		});		
 	});
 })();
+
+async function findURL(urlInfo) {
+	var year = urlInfo.year
+	var month = urlInfo.month
+	if (month != "01") {
+		month = String(parseInt(month) - 1).padStart(2, '0')
+	}
+	var fileName = urlInfo.fileName
+	var URL = 'https://dynamicpapers.com/wp-content/uploads/' + year + '/' + month + '/' + fileName;
+	var currentMonth = String(new Date().getMonth() + 1).padStart(2,"0")
+	var currentYear = String(new Date().getFullYear())
+	var found = false
+	var count = 0
+	while (found == false && count < 5) {
+		var request = await fetch(URL, {
+			"headers": {
+				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+				"accept-language": "en-US,en;q=0.9,en-GB;q=0.8",
+				"sec-ch-ua-mobile": "?0",
+				"sec-fetch-dest": "document",
+				"sec-fetch-mode": "navigate",
+				"sec-fetch-site": "none",
+				"sec-fetch-user": "?1",
+				"upgrade-insecure-requests": "1"
+			},
+			"referrerPolicy": "strict-origin-when-cross-origin",
+			"body": null,
+			"method": "HEAD",
+			"mode": "cors",
+			"credentials": "include"
+		});
+		if (request.status == 200) {
+			found = true
+			return URL;
+		}
+		else if (month == currentMonth && year == currentYear) {
+			return "";
+		}
+		else if (request.status == 404) {
+			URL = URL.split('/')
+			month = String(parseInt(month) + 1).padStart(2, '0')
+			if (parseInt(month) % 12 == 1) {
+				month = '01'
+				year = String(parseInt(year) + 1)
+				URL[URL.length - 3] = year
+			}
+			URL[URL.length - 2] = month
+			URL = URL.join("/")
+			count++
+		}
+	}
+	return ""
+}
